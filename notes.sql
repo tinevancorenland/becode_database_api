0fcc1b064c8b3f49367a990e0a855b521da1c735
tinevancorenland <tinevancorenland@gmail.com>
2019-08-08 12:12:09 +0200

    add IF NOT EXISTS and links table created to database created

--- notes.sql
+++ notes.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE DB_notes
+CREATE DATABASE IF NOT EXISTS DB_notes;
 
-CREATE TABLE notes (
+CREATE TABLE IF NOT EXISTS DB_notes.notes (
   noteID int PRIMARY KEY AUTO_INCREMENT,
   title varchar (255) NOT NULL,
   text varchar (500),
